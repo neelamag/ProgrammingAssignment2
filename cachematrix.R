f6890e0e27c465008090decd07c4c381f70721d9
neelamag <selvakumar_neelamagam@merck.com>
2016-01-23 16:06:22 -0500

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	set<-function(y){
+	x<<-y
+	m<<-NULL
+	}
+	get<-function() x
+	setmatrix<-function(solve) m<<-solve
+	getmatrix <-function() m
+	list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
@@ -12,4 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m<-x$getmatrix()
+	if(!is.null(m)){
+
+	matrix<-x$get()
+	m<-solve(matrix, ...)
+	x$setmatrix(m)
+	m
+	}
 }
